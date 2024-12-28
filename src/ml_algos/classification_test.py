1d5619a533b9f9222fe126c6658541254e24d0a6
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2024-12-27 19:20:13 -0500

    clean up code

--- src/ml_algos/classification_test.py
+++ src/ml_algos/classification_test.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 from ml_algos.model import BaseModel
 
 
-def diabetes_test(model: BaseModel, scale=False, **kwargs):
+def diabetes_test(model: BaseModel, scale=False, **training_kwargs):
     data = pd.read_csv("data/pima-indians-diabetes.csv")
     X_train, X_test, y_train, y_test = train_test_split(data, data["y"], test_size=0.33, random_state=42)
     del X_train["y"]
@@ -21,7 +21,7 @@ def diabetes_test(model: BaseModel, scale=False, **kwargs):
         for col in X_test.columns:
             X_test[col] = StandardScaler().fit_transform(X_test[col].to_numpy().reshape((-1, 1)))
 
-    model.fit(X_train, y_train, **kwargs)
+    model.fit(X_train, y_train, **training_kwargs)
     print(accuracy_score(y_test, np.where(model.predict(X_test) > 0.5, 1, 0)))
 
     plt.scatter(X_test.index, y_test)
@@ -29,10 +29,14 @@ def diabetes_test(model: BaseModel, scale=False, **kwargs):
     plt.show()  
 
 
-def iris_test(model: BaseModel, scale=False, binary=None, **kwargs):
+def iris_test(model: BaseModel, scale=False, binary=None, one_hot_encode=False, **training_kwargs):
     df = pd.read_csv('data/iris.csv')
     if binary:
         df["variety"] = df["variety"].apply(lambda x: 1 if x == binary else -1)
+    elif one_hot_encode:
+        df["variety"] = df["variety"].apply(
+            lambda x: np.array([1, 0, 0]) if x == "Setosa" else np.array([0, 1, 0]) if x == "Virginica" else np.array([0, 0, 1])
+        )
 
     X_train, X_test, y_train, y_test = train_test_split(df, df["variety"], test_size=0.33, random_state=42)
     del X_train["variety"]
@@ -44,8 +48,19 @@ def iris_test(model: BaseModel, scale=False, binary=None, **kwargs):
         for col in X_test.columns:
             X_test[col] = StandardScaler().fit_transform(X_test[col].to_numpy().reshape((-1, 1)))
     
-    model.fit(X_train, y_train)
+    if one_hot_encode:
+        y_train = np.array(list(map(lambda x: x.tolist(), y_train.to_numpy())))
+    
+    model.fit(X_train, y_train, **training_kwargs)
     test_result = model.predict(X_test)
+
+    if one_hot_encode:
+        y_test = np.array(list(map(lambda x: x.tolist(), y_test.to_numpy())))
+        test_result = np.argmax(test_result, axis=1)
+        y_test = np.argmax(y_test, axis=1)
+        print("accuracy score:", accuracy_score(test_result, y_test))
+        print("confusion matrix:\n", confusion_matrix(test_result, y_test))
+        return
     
     print("accuracy score:", accuracy_score(test_result.to_numpy(), y_test.to_numpy()))
     print("confusion matrix:\n", confusion_matrix(test_result.to_numpy(), y_test.to_numpy()))
--- src/ml_algos/neural_network.py
+++ src/ml_algos/neural_network.py
@@ -1,103 +1,120 @@
 import numpy as np
 import pandas as pd
 
+from tqdm import trange
+
+from ml_algos.classification_test import iris_test
 from ml_algos.model import BaseModel
 
-from matplotlib import pyplot as plt
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import math
+
+from enum import Enum
 
 
 class MultiLayerPerceptron(BaseModel):
-    def __init__(self, num_layers, input_features, layer_sizes, activations, activation_derivatives, loss, loss_gradient, learning_rate):
-        self.num_layers = num_layers
-        self.input_features = input_features
-        self.layer_sizes = layer_sizes
-        w = [self.input_features] + layer_sizes
-        self.W = [
-            np.random.normal(0, 0.05, size=(w[i] + 1, w[i + 1])) for i in range(len(w) - 1)
-        ]
-        self.activations = activations
-        self.activation_derivatives = activation_derivatives
-        self.loss = loss
-        self.loss_gradient = loss_gradient
-        self.learning_rate = learning_rate
-        print([w.shape for w in self.W])
-        super(MultiLayerPerceptron, self).__init__(verbose=True)
+    class Activation(Enum):
+        SIGMOID = {
+            "function": lambda x: 1 / (1 + np.exp(-x)),
+            "gradient": lambda x: (1 / (1 + np.exp(-x))) * (1 - (1 / (1 + np.exp(-x))))
+        }
     
-    def forward(self, X: pd.DataFrame):
+    class Loss(Enum):
+        MEAN_SQUARED_ERROR = {
+            "function": lambda x, y: np.mean((x - y) ** 2),
+            "gradient": lambda x, y: 2 * (x - y),
+        }
+    
+    class WeightInitialization(Enum):
+        GLOROT = lambda n, size: np.random.uniform(-1 / math.sqrt(n), 1 / math.sqrt(n), size=size)
+        HE = lambda n, size: np.random.normal(0, 2 / n, size=size)
+    
+    class BackwardMethod(Enum):
+        BATCH_GRADIENT_DESCENT = 0
+        STOCHASTIC_GRADIENT_DESCENT = 1
+
+    def __init__(
+        self, 
+        num_input_features: int, 
+        layer_sizes: list[int], 
+        activations: list[Activation], 
+        loss: Loss,
+        weight_initialization: WeightInitialization = WeightInitialization.GLOROT,
+        backward_method: BackwardMethod = BackwardMethod.BATCH_GRADIENT_DESCENT,
+        verbose=False
+    ):
+        self.num_input_features = num_input_features
+        self.layer_sizes = layer_sizes
+        self.weight_initialization = weight_initialization
+        self.backward_method = backward_method
+
+        w = [self.num_input_features] + layer_sizes
+        self.W = [
+            self.weight_initialization(w[i] + 1, (w[i] + 1, w[i + 1])) for i in range(len(w) - 1)
+        ]
+
+        self.activations = [func.value["function"] for func in activations]
+        self.activation_gradients = [func.value["gradient"] for func in activations]
+        self.loss = loss.value["function"]
+        self.loss_gradient = loss.value["gradient"]
+        self.num_layers = len(self.layer_sizes)
+
+        super(MultiLayerPerceptron, self).__init__(verbose=verbose)
+    
+    def __forward(self, X: pd.DataFrame):
         X_current = X.to_numpy()
         X_current = np.concatenate((X_current, np.ones((X_current.shape[0], 1))), axis=1)
         self.pre_activations = []
         self.post_activations = [X_current]
+
         for i in range(self.num_layers):
             self.pre_activations.append(X_current @ self.W[i])
-            X_current = np.concatenate((self.activations[i](self.pre_activations[-1]), np.ones((self.post_activations[-1].shape[0], 1))), axis=1)
+            X_current = np.concatenate((
+                self.activations[i](self.pre_activations[-1]), 
+                np.ones((self.post_activations[-1].shape[0], 1))), 
+            axis=1)
             self.post_activations.append(X_current)
+
         return self.post_activations[-1][:, :-1]
     
-    def backward(self, H: np.ndarray, y: np.ndarray):
+    def __backward(self, H: np.ndarray, y: np.ndarray, learning_rate: float):
         dldH = self.loss_gradient(H, y)
-        dldZ = dldH * self.activation_derivatives[-1](self.pre_activations[-1])
+        dldZ = dldH * self.activation_gradients[-1](self.pre_activations[-1])
         weight_gradients = []
+
         for i in range(1, self.num_layers + 1):
             dldW = (1 / H.shape[0]) * self.post_activations[-i - 1].T @ dldZ
             weight_gradients.append(dldW)
             if i < self.num_layers:
                 dldH_1 = dldZ @ self.W[-i].T
                 dldH = dldH_1[:, :-1]
-                dldZ = dldH * self.activation_derivatives[-i - 1](self.pre_activations[-i - 1])
+                dldZ = dldH * self.activation_gradients[-i - 1](self.pre_activations[-i - 1])
+        
         weight_gradients = weight_gradients[::-1]
         for i in range(len(self.W)):
-            self.W[i] = self.W[i] - self.learning_rate * weight_gradients[i]
+            self.W[i] = self.W[i] - learning_rate * weight_gradients[i]
         return self.loss(H, y)
     
-    def __train(self, X, y):
-        H = self.forward(X)
-        return self.backward(H, y)
+    def __train(self, X: pd.DataFrame, y: np.ndarray, learning_rate):
+        H = self.__forward(X)
+        return self.__backward(H, y, learning_rate)
 
-    def _fit(self, X, y, iterations=100):
-        for i in range(iterations):
-            loss = self.__train(X, y)
-            if self.verbose:
-                print(f"iteration {i + 1}/{iterations}: MSE", loss)
+    def _fit(self, X: pd.DataFrame, y: np.ndarray, iterations=100, learning_rate=0.1):
+        with trange(iterations) as pbar:
+            for _ in pbar:
+                loss = self.__train(X, y, learning_rate)
+                pbar.set_description(f"Loss: {loss:.4f}")
     
-    def _predict(self, X):
-        return self.forward(X)
+    def _predict(self, X: pd.DataFrame):
+        return self.__forward(X)
 
 
 if __name__ == "__main__":
-    df = pd.read_csv('data/iris.csv')
-    df["variety"] = df["variety"].apply(
-        lambda x: np.array([1, 0, 0]) if x == "Setosa" else np.array([0, 1, 0]) if x == "Virginica" else np.array([0, 0, 1])
-    )
-
-    X_train, X_test, y_train, y_test = train_test_split(df, df["variety"], test_size=0.33, random_state=42)
-    del X_train["variety"]
-    del X_test["variety"]
-
-    for col in X_train.columns:
-        X_train[col] = StandardScaler().fit_transform(X_train[col].to_numpy().reshape((-1, 1)))
-    for col in X_test.columns:
-        X_test[col] = StandardScaler().fit_transform(X_test[col].to_numpy().reshape((-1, 1)))
-
     model = MultiLayerPerceptron(
-        2,
         4,
         [5, 3],
-        [lambda x: 1 / (1 + np.exp(-x)) for _ in range(3)],
-        [lambda x: (1 / (1 + np.exp(-x))) * (1 - (1 / (1 + np.exp(-x)))) for _ in range(3)],
-        lambda x, y: np.mean((x - y) ** 2),
-        lambda x, y: 2 * (x - y),
-        0.1
+        [MultiLayerPerceptron.Activation.SIGMOID for _ in range(3)],
+        MultiLayerPerceptron.Loss.MEAN_SQUARED_ERROR,
     )
-    
-    model.fit(X_train, np.array(list(map(lambda x: x.tolist(), y_train.to_numpy()))), iterations=1000)
-    test_result = model.predict(X_test)
 
-    y_test = np.array(list(map(lambda x: x.tolist(), y_test.to_numpy())))
-    
-    print("accuracy score:", accuracy_score(np.argmax(test_result, axis=1), np.argmax(y_test, axis=1)))
-    print("confusion matrix:\n", confusion_matrix(np.argmax(test_result, axis=1), np.argmax(y_test, axis=1)))
+    iris_test(model, scale=True, one_hot_encode=True, iterations=1000, learning_rate=0.1)
     
